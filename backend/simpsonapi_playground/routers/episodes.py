227d983e3ded8c72d445153491dc2ac3c5f2fb08
Zh3ka21 <puzanov_zhenechk@ukr.net>
2026-01-26 23:06:25 +0000

    chore(fix): routes fixed up to rest, add season router

--- backend/simpsonapi_playground/routers/episodes.py
+++ backend/simpsonapi_playground/routers/episodes.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 from simpsonapi_playground.core.db import get_db
 from simpsonapi_playground.schemas.episodes_schemas import (
@@ -11,7 +11,6 @@ from simpsonapi_playground.crud.episode import (
     get_episode,
     get_episodes,
     get_episode_by_name,
-    get_episodes_by_season,
     get_season_by_episode,
     put_episode,
     del_episode,
@@ -21,35 +20,32 @@ from simpsonapi_playground.crud.episode import (
 router = APIRouter(prefix="/episodes", tags=["episodes"])
 
 
-@router.post("/", response_model=EpisodeSchema)
-def create_episode_router(character: EpisodeCreate, db: Session = Depends(get_db)):
-    return create_episode(db, character)
+@router.post("/", response_model=EpisodeSchema, status_code=status.HTTP_201_CREATED)
+def create_episode_router(episode: EpisodeCreate, db: Session = Depends(get_db)):
+    return create_episode(db, episode)
 
 
-@router.get("/id/{episode_id}", response_model=EpisodeSchema)
+@router.get("/{episode_id}", response_model=EpisodeResponse)
 def get_episode_router(episode_id: int, db: Session = Depends(get_db)):
     db_episode = get_episode(db, episode_id)
-    if not db_episode:
-        raise HTTPException(status_code=404, detail="Character not found")
-    return db_episode
-
-
-@router.get("/", response_model=list[EpisodeResponse])
-def get_episodes_router(db: Session = Depends(get_db)):
-    return get_episodes(db)
-
-
-@router.get("/name/{episode_title}", response_model=EpisodeResponse)
-def get_episode_by_name_router(episode_title: str, db: Session = Depends(get_db)):
-    db_episode = get_episode_by_name(db, episode_title)
     if not db_episode:
         raise HTTPException(status_code=404, detail="Episode not found")
     return db_episode
 
 
+@router.get("/", response_model=list[EpisodeSchema])
+def get_episodes_router(db: Session = Depends(get_db), title: str | None = None):
+    if title:
+        episode = get_episode_by_name(db, title)
+        if not episode:
+            raise HTTPException(status_code=404, detail="Episode not found")
+        return [episode]
+    return get_episodes(db)
+
+
 @router.put("/{episode_id}", response_model=EpisodeResponse)
 def update_episode_router(
-    episode_id: int = None, data: EpisodeCreate = None, db: Session = Depends(get_db)
+    episode_id: int, data: EpisodeCreate, db: Session = Depends(get_db)
 ):
     upd_episode = put_episode(db, episode_id, data)
     if not upd_episode:
@@ -57,24 +53,17 @@ def update_episode_router(
     return upd_episode
 
 
-@router.delete("/{episode_id}", status_code=204)
-def delete_character(episode_id: int = None, db: Session = Depends(get_db)):
+@router.delete("/{episode_id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_episode(episode_id: int = None, db: Session = Depends(get_db)):
     del_episode(db, episode_id)
 
 
 # TODO: episode description/request to get more details on episodes
-@router.get("/season/{season_id}", response_model=list[EpisodeResponse])
-def get_episodes_by_season_router(season_id: int = None, db: Session = Depends(get_db)):
-    episodes = get_episodes_by_season(db, season_id)
-    return episodes
-
-
-# TODO: episode description/request to get more details on episodes
-@router.get("/episode/{episode_id}", response_model=list[EpisodeResponse])
-def get_season_by_episode_router(episode_id: int = None, db: Session = Depends(get_db)):
+@router.get("/{episode_id}/season")
+def get_season_by_episode_router(episode_id: int, db: Session = Depends(get_db)):
     return get_season_by_episode(db, episode_id)
 
 
-@router.get("/episode/", response_model=EpisodeResponse)
+@router.get("/random", response_model=EpisodeResponse)
 def get_random_episode_router(db: Session = Depends(get_db)):
     return select_random_episode(db)
